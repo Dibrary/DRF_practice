cb35ff0cbf64f1da01414210c9ab6f0be28fa9d6
Dibrary <kkdh828@hanmail.net>
2022-09-17 23:26:56 +0900

    220917 serializer에 대한 코드공부 시작

--- myweb/urls.py
+++ myweb/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('photo.urls')),
+    path('', include('books.urls')),
+    path('photo/', include('photo.urls')),
 ]
